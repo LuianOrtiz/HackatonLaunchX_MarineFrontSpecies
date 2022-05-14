852c3cc72723da8f9256fd2ce0e6a7681feb37a0
Luian Ortiz <luiann.ortiz@gmail.com>
2022-05-14 08:15:39 -0500

    add view's and components routes

---
+++ src/views/AdminView.vue
@@ -0,0 +1,17 @@
+<template>
+    
+</template>
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+    setup() {
+        
+    },
+})
+</script>
+
+<style scoped>
+
+</style>
