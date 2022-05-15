c4019bdbb526ad52e4f99dfb66f11324565099ad
Fernando Arceo Olivares <barakaldo188@gmail.com>
2022-05-15 15:47:39 -0500

    Modified: Homepage
    Modified homepage banner, homepage cards, footer info, page title & GitHub links.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,11 +4,12 @@
     <br>
     <PurposeObjective 
       titulo1="Objetivo"
-      parrafo1="Stet sd erat eu takimata eum aliquyam amet vel et invidunt facilisis voluptua gubergren"
-      parrafo2="Te e ipsum sed sit ipsum sed sea diam voluptua lobortis diam esse elitr"
-      titulo2="Otra cosa"
-      parrafo3="Dolores taio stet sanctus vulputate vero nonumy justo ipsum sit elitr diam illum vero"
-      parrafo4="Et nomy invidunt et kasd dolor eirmod takimata assum justo duo nonumy eos sanctus"
+      parrafo1="Darle visibilidad a las especies marinas que se encuentran en peligro de extinción en nuestro país."
+      parrafo2="El nombre de nuestro proyecto es 'Marine Well' haciendo alusión a un pozo de sabiduría marina del cual cualquier persona puede acercarse a beber para así ayudar a la protección de los animales que aquí se muestran dentro de la pestaña ‘Animales’. Ayúdanos a que este pozo de conocimiento se vacié para que los océanos estén llenos de todas estas grandiosas especies.
+"
+      titulo2="Escalabilidad"
+      parrafo3="La página cuenta con la capacidad de añadir más especies en peligro desde la pestaña de administración."
+      parrafo4="En siguientes versiones el proyecto es capaz de crecer para mostrar información acerca de animales marinos no solamente de especies mexicanas pudiendo así ser una fuente de información para más gente alrededor del mundo."
     />
     <br><br>
     
